4801c69a20a796c1ef25a0c6a9297ad88579a5c6
Nuno Silva <nuno.silva@arxi.pt>
2021-03-26 15:03:13 +0000

    [ADD] sale_country_badge module

---
+++ sale_country_badge/models/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    country = fields.Char(readonly=True)
+
+    def write(self, values):
+        if values.get('state') == 'sent':
+            self.country = self.partner_id.country_id.name
+        return super(SaleOrder, self).write(values)
